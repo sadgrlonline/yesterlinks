b9fb1289e6df072cc9039c63cb9a0349600aa793
Sadness <95544008+sadgrlonline@users.noreply.github.com>
2022-03-10 10:14:32 -0500

    added nav template

---
+++ admin/navigation.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/yesterlinks/">Home</a></li>
+        <li><a href="/yesterlinks/admin/approve.php">Approve</a></li>
+        <li><a href="/yesterlinks/submit-a-link.php">Submit</a></li>
+        <li><a href="/yesterlinks/admin/">Edit</a></li>
+    </ul>
+</nav>
